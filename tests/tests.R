034ed1beff2a5d71f82ff03cc2b694588ebf7034
zhanxw <zhanxw@gmail.com>
2017-04-05 12:39:30 -0400

    update for testthat

--- tests/tests.R
+++ tests/tests.R
@@ -10,7 +10,8 @@ print(Sys.info())
 print(citation("seqminer"))
 
 library(testthat)
-library(seqminer)
-## test code are under inst/tests
-## test_package("seqminer", reporter="tap")
-test_package("seqminer")
+## library(seqminer)
+## ## test code are under inst/tests
+## ## test_package("seqminer", reporter="tap")
+## test_package("seqminer")
+test_check("seqminer")
